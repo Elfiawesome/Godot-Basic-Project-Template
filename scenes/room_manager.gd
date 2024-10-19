ff3d4c51f335b449217175d2a35c7222058ba7be
Elfiawesome23 <elfiawesome23@gmail.com>
2024-10-19 22:47:10 +0800

    Initial Commit

---
+++ scenes/room_manager.gd
@@ -0,0 +1,35 @@
+class_name RoomManager extends Control
+@export var room_nodes:Control
+@export var transition_nodes:Control
+
+enum ROOM_ID {
+	MAIN_MENU
+}
+var _room_scenes:Dictionary = {}
+var current_room:Room
+
+func _ready() -> void:
+	global.transition_manager.transition_nodes = transition_nodes
+	# Register rooms here
+	_room_scenes[ROOM_ID.MAIN_MENU] = load("res://scenes/rooms/main_menu/main_menu.tscn")
+	change_room(ROOM_ID.MAIN_MENU, global.transition_manager.FADE)
+
+func change_room(room_id: int, transition_id:int = TransitionManager.NONE, transition_params:Dictionary = {}, completion_lamda: Callable = func(_room: Room) -> void:pass) -> void:
+	var new_room:Room = instantiate_room(room_id)
+	global.transition_manager.start_transition(
+		transition_id, {},
+		func() -> void:
+			if current_room:
+				current_room.queue_free()
+			room_nodes.add_child(new_room)
+			current_room = new_room
+			completion_lamda.call(current_room)
+	)
+
+func instantiate_room(room_id: int) -> Room:
+	if _room_scenes.has(room_id):
+		var loaded_scene:PackedScene = _room_scenes[room_id]
+		var scene:Room = loaded_scene.instantiate()
+		scene.room_manager = self
+		return scene
+	return
